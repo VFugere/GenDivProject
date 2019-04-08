7475576f51f863cc0568701fcfea287f145e20f7
V_Fugere <vincent.fugere@mail.mcgill.ca>
2019-04-08 19:40:46 -0400

    starting adding human impact variables

--- Merge_and_format.R
+++ Merge_and_format.R
@@ -1,14 +1,15 @@
 ## Vincent Fugere 2019
 
-# This code unites the 3 population-level dataset: π, D, and
-# land use and human density variables.
+# This code unites population-level datasets and create a master data file for analysis
 # Also explores the number of populations and times series at each scale
 
 rm(list=ls())
 library(tidyverse)
+library(vegan)
 
 load('~/Desktop/Data/DF_D.Rdata')
 load('~/Desktop/Data/DF_Pi.Rdata')
+load('~/Desktop/Data/sequence_metadata.RData')
 
 DF_Pi <- select(DF_Pi, -(taxon:species))
 DF_Pi$year <- as.numeric(DF_Pi$year)
@@ -30,15 +31,21 @@ alldata %>% filter(n.years >= 3) %>%
 #that we are not simply cutting pops into small units
 
 #get spatial centroids of pops
-load('~/Desktop/Data/sequence_metadata.RData')
 sc10 <- seq %>% group_by(pop10) %>% summarize('nseqs' = n(),'lat' = mean(lat), 'long' = mean(long)) %>% rename('pop' = pop10)
 sc100 <- seq %>% group_by(pop100) %>% summarize('nseqs' = n(),'lat' = mean(lat), 'long' = mean(long)) %>% rename('pop' = pop100)
 sc1000 <- seq %>% group_by(pop1000) %>% summarize('nseqs' = n(),'lat' = mean(lat), 'long' = mean(long)) %>% rename('pop' = pop1000)
 sc10000 <- seq %>% group_by(pop10000) %>% summarize('nseqs' = n(),'lat' = mean(lat), 'long' = mean(long)) %>% rename('pop' = pop10000)
 sc100000 <- seq %>% group_by(pop100000) %>% summarize('nseqs' = n(),'lat' = mean(lat), 'long' = mean(long)) %>% rename('pop' = pop100000)
 centroids <- bind_rows(sc10,sc100,sc1000,sc10000,sc100000) %>% filter(nseqs > 1) %>% select(-nseqs)
-rm(seq,sc10,sc100,sc1000,sc10000,sc100000)
+rm(sc10,sc100,sc1000,sc10000,sc100000)
+alldata <- left_join(alldata, centroids, by = 'pop')
 
-
-
-
+#add human impact variables
+sc10 <- seq %>% group_by(pop10,year) %>% summarize('nseqs' = n(),'hd' = mean(pop_tot), 'p.lu' = mean(conv_rangeland+cropland+pasture+urban,na.rm = T), 'lu.div' = exp(diversity(c(.$conv_rangeland,.$rangeland,.$urban),index='shannon'))) %>% rename('pop' = pop10)
+sc100 <- seq %>% group_by(pop100,year) %>% summarize('nseqs' = n(),'lat' = mean(lat), 'long' = mean(long)) %>% rename('pop' = pop100)
+sc1000 <- seq %>% group_by(pop1000,year) %>% summarize('nseqs' = n(),'lat' = mean(lat), 'long' = mean(long)) %>% rename('pop' = pop1000)
+sc10000 <- seq %>% group_by(pop10000,year) %>% summarize('nseqs' = n(),'lat' = mean(lat), 'long' = mean(long)) %>% rename('pop' = pop10000)
+sc100000 <- seq %>% group_by(pop100000,year) %>% summarize('nseqs' = n(),'lat' = mean(lat), 'long' = mean(long)) %>% rename('pop' = pop100000)
+human.impacts <- bind_rows(sc10,sc100,sc1000,sc10000,sc100000) %>% filter(nseqs > 1) %>% select(-nseqs)
+rm(sc10,sc100,sc1000,sc10000,sc100000)
+alldata <- left_join(alldata, human.impacts, by = 'pop')
