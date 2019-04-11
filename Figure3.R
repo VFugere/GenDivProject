d2582833de754c858914e080361119e54786ceda
V_Fugere <vincent.fugere@mail.mcgill.ca>
2019-04-11 16:10:57 -0400

    more gam

--- Figure3.R
+++ Figure3.R
@@ -17,7 +17,7 @@ library(sp)
 library(tictoc)
 
 plot.s <- function(model,xvar,cond,col,xlab,ylab){
-  plot_smooth(model, view=xvar, cond=cond, col=col, rm.ranef=T, hide.label = T,xlab=xlab,ylab=ylab,main=NULL,rug=F,bty='l',legend_plot_all = F, h0=NA)
+  plot_smooth(model, view=xvar, cond=cond, col=col, rm.ranef=T, hide.label = T,xlab=xlab,ylab=ylab,main=NULL,rug=F,bty='l',legend_plot_all = F, h0=NA, transform=exp)
 }
 
 map <- getMap(resolution = "coarse")
@@ -25,10 +25,10 @@ map <- getMap(resolution = "coarse")
 load('~/Google Drive/Recherche/Intraspecific genetic diversity/Data/DF_Master.RData')
 
 #start with a single scale
-scale.fun <-function(x){y <- scales::rescale(x, to = c(0,1)); return(y)}
+scale.fun <-function(x){y <- scales::rescale(log1p(x), to = c(0,1)); return(y)}
 temp <- DF %>% filter(scale == '1000', taxon == 'birds')
 temp <- temp %>% mutate_at(vars(D:lu.div), scale.fun)
-temp %<>% filter(nseqs >= 10)
+#temp %<>% filter(nseqs >= 10)
 temp %<>% mutate('lat.sq' = scale.fun(lat^2), 'wts' = log(nseqs)/mean(log(nseqs)))
 temp %<>% select(-taxon,-scale,-nseqs, -ncomps,-n.years, -lu.var) %>%
   mutate_at(vars(pop,year,species), as.factor) %>% as.data.frame
@@ -36,19 +36,37 @@ temp %<>% filter(div < mean(div)+10*sd(div))
 
 #smoothed genetic diversity, accounting for D and Yr
 
-m1 <- bam(div ~ s(lat,long, k = 200, m = 1) + s(D, k = 20, bs = 'cr') + s(year,bs = 're', k = 1, m=2), data = temp, family = tw, method='fREML', discrete = T)
+
+mod0 <- bam(div ~ s(lat,long, k = 15, m = 1) + s(D, k = 10, bs = 'tp'), data = temp, family = tw, method='fREML', discrete = T)
+p <- str_split(family(mod0)[[1]], '=', simplify = T)[1,2] %>% str_remove('\\)') %>% as.numeric
+  
+
+# tic()
+# m1 <- bam(div ~ s(lat,long, k = 15, m = 1) + s(D, k = 10, bs = 'tp') + s(year,bs = 're', k = 5, m=2) +
+#             s(lat.sq, k = 15, bs = 'tp') + s(hd, k=15, bs ='tp') + s(hd.var, k=10, bs='tp') +
+#             s(p.lu, k=10, bs='tp') + s(lu.div, k=10, bs='tp'),
+#           data = temp, family = Tweedie(p = p), method='fREML', discrete = T, weights = wts)
+# toc()
+
+tic()
+m1 <- bam(div ~ s(lat,long, k = 15, m = 1) + s(D, k = 10, bs = 'tp') + s(year,bs = 're', k = 5, m=2) +
+            s(lat.sq, k = 15, bs = 'tp') + s(hd, k=15, bs ='tp') +
+            s(p.lu, k=10, bs='tp') + s(species, bs='re', k =5, m=2),
+          data = temp, family = Tweedie(p = p), method='fREML', discrete = T, weights = wts)
+toc()
+
+
+summary(m1)
 gam.check(m1)
 
-tic()
-m1 <- bam(div ~ s(lat,long, k = 15, m = 1) + s(D, k = 10, bs = 'cr') + s(year,bs = 're', k = 10, m=2), data = temp, family = tw, method='fREML', discrete = T)
-toc()
+par(mfrow=c(2,3),cex=1)
+plot.s(m1,'D',list('lat' = 0, 'long' = 0),1,'mean spatial distance (D)',expression(hat(pi)))
+plot.s(m1,'lat.sq',list('lat' = 0, 'long' = 0),1,expression(Latitude^2),expression(hat(pi)))
+plot.s(m1,'hd',list('lat' = 0, 'long' = 0),1,expression(human~density~(people~km^-2)),expression(hat(pi)))
+plot.s(m1,'hd.var',list('lat' = 0, 'long' = 0),1,expression(variance~'in'~human~density~(people~km^-2)),expression(hat(pi)))
+plot.s(m1,'p.lu',list('lat' = 0, 'long' = 0),1,expression(land~use~intensity),expression(hat(pi)))
+plot.s(m1,'lu.div',list('lat' = 0, 'long' = 0),1,expression(land~use~heterogeneity),expression(hat(pi)))
 
-tic()
-m1 <- bam(div ~ s(lat,long, k = 15, m = 1) + s(D, k = 10, bs = 'cr') + s(year,bs = 're', k = 10, m=2), data = temp, family = Tweedie(p = 1.9), method='fREML', discrete = T)
-toc()
-
-plot.s(m1,'D',list('lat' = 0, 'long' = 0),1,'D','div')
-points(log(div)~D,temp,pch=16)
 
 cols <- rev(c('#d73027','#f46d43','#fdae61','#fee090','#e0f3f8','#abd9e9','#74add1','#4575b4'))
 colfunc <- colorRampPalette(cols)
@@ -71,16 +89,6 @@ rect(xleft=xseqs,xright=xseqs+(xseqs[2]-xseqs[1]),ybottom = rep(-55,8),ytop = re
 segments(x0=xseqs[2:8],x1=xseqs[2:8],y0=rep(-50,7),y1=c(-59,-57,-57,-59,-57,-57,-59),lwd=c(0.5,0.3,0.3,0.5,0.3,0.3,0.5))
 text(x=xseqs[c(2,5,8)],y=rep(-59,3),labels = legs[c(2,5,8)],pos=1)
 text(x=27.85714,y=-49,cex=1,label=smoothed~COI~pi,pos=3)
-
-tic()
-m1 <- bam(div ~ s(lat,long, bs='gp', k = 50, m = 1) + s(D, bs='cr', k = 20) + s(year,bs = 're', k = 40) + s(species,bs = 're', k = 2), data = temp, family = tw, method='fREML')
-toc()
-
-m1 <- bam(div ~ s(lat,long) + s(lat.sq) + s(D) + s(hd) + s(hd.var) + s(p.lu) + s(lu.div) + s(year, bs='re') + s(species, bs = 're'),
-          data = temp, family = tw, method='fREML',cluster=cl)
-
-summary(m1)
-gam.check(m1)
 
 
 #spatial autocorrelation in residuals?
