48a96673161759c604161bc146d7c1c39c35de8d
V_Fugere <vincent.fugere@mail.mcgill.ca>
2019-04-16 19:19:34 -0400

    figuring out the best gam structure

--- 17. Temporal GAMMs.R
+++ 17. Temporal GAMMs.R
@@ -16,7 +16,7 @@ load('~/Google Drive/Recherche/Intraspecific genetic diversity/Data/DF_Master.RD
 
 #parameters
 min.nb.seqs <- 2
-min.nb.years <- 3
+min.nb.years <- 5
 taxa <- c('birds','fish','insects','mammals')
 #scales <- c('10','100','1000','10000')
 scl <- '1000'
@@ -40,12 +40,36 @@ for(tax in taxa){
     mutate('wts' = log(nseqs)/mean(log(nseqs))) %>%
     as.data.frame
   
-  tsmod <- bam(div ~ s(lat,long, bs='gp', k = 50) + s(D, k = 8, bs = 'tp') + s(year, k = 8, bs = 'tp') +
-                 ti(year,hd, k = 8) + ti(year,p.lu, k = 8) + s(year,pop, bs = 'fs', k = 5, m = 1),
+  tsmod2 <- bam(div ~ s(lat,long, bs='gp', k = 50) + s(D, k = 8, bs = 'tp') + s(year, k = 8, bs = 'tp') +
+                 s(hd, k = 6, bs = 'tp') + s(p.lu, k = 6, bs = 'tp') +
+                 ti(year,hd, k = 6) + ti(year,p.lu, k = 6) + s(year,pop, bs = 'fs', k = 5, m = 1),
                data = temp, family = tw, method='fREML', discrete = T, weights = wts)
   
-  # summary(tsmod)
-  # gam.check(tsmod)
+  summary(tsmod2)
+  anova(tsmod2)
+  gam.check(tsmod2)
+  
+  fvisgam(tsmod2, view = c('year','p.lu'), cond = list('hd' = 0), ylab='land use',add.color.legend=F,hide.label=T,xlab = 'year',plot.type = 'contour', color = viridis(50), main = NULL)
+  pvisgam(tsmod2, view = c('year','p.lu'), ylab='land use',add.color.legend=F,hide.label=T,xlab = 'year',plot.type = 'contour', color = viridis(50), main = NULL)
+  pvisgam(tsmod2, view = c('year','hd'), ylab='land use',add.color.legend=F,hide.label=T,xlab = 'year',plot.type = 'contour', color = viridis(50), main = NULL)
+  
+  plot_smooth(tsmod2, view="year", cond=list('hd' = 0,'p.lu'= 0.1), col=1, rm.ranef=T, se=1.96, yaxt='n',xaxt='n',ann=F, hide.label = T,lwd=2,main=NULL,rug=F,bty='l',legend_plot_all = F, h0=NA)
+  plot_smooth(tsmod2, view="year", cond=list('hd' = 0,'p.lu' = 0.9), lwd=2,col='darkorchid1', add=T,rm.ranef=T, se=1.96,rug=F)
+  
+  inspect_random(tsmod2)
+  
+  pvisgam(tsmod2, view = c('year','hd'), cond = list('p.lu' = 0), ylab='human density',add.color.legend=F,hide.label=T,xlab = 'year',plot.type = 'contour', color = viridis(50), main = NULL)
+  plot_smooth(tsmod2, view="year", cond=list('hd' = 0.1,'p.lu'= 0), lwd=2,col=1, rm.ranef=T, se=1.96, yaxt='n',xaxt='n',ann=F, hide.label = T,main=NULL,rug=F,bty='l',legend_plot_all = F, h0=NA)
+  plot_smooth(tsmod2, view="year", cond=list('hd' = 0.9,'p.lu' = 0), lwd=2,col='darkorange1', add=T,rm.ranef=T, se=1.96,rug=F)
+  
+  viridis(50)[1] -> col_ln
+  plot_smooth(tsmod2, view="year", plot_all=c('pop'),col=alpha(1,0.2), rm.ranef=F, se=0, yaxt='n',xaxt='n',ann=F, hide.label = T,main=NULL,rug=F,bty='l',legend_plot_all = F, h0=NA)
+  plot_smooth(tsmod2, view="year", lwd=3, col=col_ln, rm.ranef=T, se=1.96, rug=F, add=T)
+  
+  plot_smooth(tsmod2, view="year", lwd=3, col=col_ln, rm.ranef=T, se=1.96, rug=F, add=F)
+  
+  plot_smooth(tsmod2, view="year", plot_all=c('pop'),add=T,col=alpha(1,0.2), rm.ranef=F, se=0, yaxt='n',xaxt='n',ann=F, hide.label = T,main=NULL,rug=F,bty='l',legend_plot_all = F, h0=NA)
+  
   
   modname <- paste('m1',tax,scl,sep='_')
   assign(modname,tsmod)
