d3ee80192d6f9f8a83ac3089d3e08e8666ec922e
V_Fugere <vincent.fugere@mail.mcgill.ca>
2019-08-08 11:42:10 -0400

    fixed n.years filtration, added 2-way interactions in Model

--- TemporalGLMMs.R
+++ TemporalGLMMs.R
@@ -28,7 +28,12 @@ models <- list()
 for(tax in taxa){
   
   temp <- DF %>% filter(scale == scl, taxon == tax)
-  temp %<>% filter(nseqs >= min.nb.seqs, div < mean(div)+10*sd(div), n.years >= min.nb.years) %>%
+  temp %<>% filter(nseqs >= min.nb.seqs, div < mean(div)+10*sd(div)) %>%
+    add_count(pop) %>%
+    select(-n.years) %>%
+    rename('n.years' = n) %>%
+    select(taxon:ncomps,n.years,lat:lu.div) %>%
+    filter(n.years > min.nb.years) %>%
     mutate('year' = as.numeric(year))
   
   #removing duplicate pops per species because nb species == nb pops (almost),
@@ -51,6 +56,8 @@ for(tax in taxa){
                     year +
                     hd +
                     p.lu +
+                    year:hd +
+                    year:p.lu +
                     (1+year|species) +
                     (1|order/family),
                   data = temp,
@@ -78,7 +85,7 @@ viridis(100)[1] -> col_ln
 par(mfrow=c(2,2),cex=1,mar=c(2,2,1,1),oma=c(3,3,0,0))
 
 xlims <- rbind(c(1985,2015),c(1994,2016),c(1988,2016),c(1984,2015))
-ylims <- rbind(c(-9,-4),c(-7,-3),c(-12,-4),c(-7,-3))
+ylims <- rbind(c(-8,-2),c(-8,-3),c(-10,-1),c(-6.5,-2.5))
 
 for(i in 1:4){
   
@@ -100,11 +107,11 @@ for(i in 1:4){
   for(focalsp in levels(spmod$frame$species)){
     spdat <- spmod$frame %>% filter(species == focalsp)
     xs <- seq(min(spdat$year),max(spdat$year),by=0.01)
-    conditions <- expand.grid(lat=0,
-                              long=0,
-                              D=0,
-                              hd=0,
-                              p.lu=0,
+    conditions <- expand.grid(lat=median(spmod$frame$lat),
+                              long=median(spmod$frame$long),
+                              D=median(spmod$frame$D),
+                              hd=median(spmod$frame$hd),
+                              p.lu=median(spmod$frame$p.lu),
                               year=xs,
                               species=focalsp,
                               order=spdat$order[1],
@@ -113,8 +120,36 @@ for(i in 1:4){
     points(log(ys)~xs,col=alpha(1,ln.alpha),type='l')
   }
   xs <- seq(min(spmod$frame$year),max(spmod$frame$year),by=0.01)
-  ys <- fixef(spmod)[1] + fixef(spmod)[5]*xs
+  ys <- fixef(spmod)[1] +
+    median(spmod$frame$lat)*fixef(spmod)[2] +
+    median(spmod$frame$long)*(fixef(spmod)[3] + median(spmod$frame$lat)*fixef(spmod)[8])+
+    median(spmod$frame$D)*fixef(spmod)[4] +
+    fixef(spmod)[5]*xs +
+    median(spmod$frame$hd)*(fixef(spmod)[6] + xs*fixef(spmod)[9]) +
+    median(spmod$frame$p.lu)*(fixef(spmod)[7] + xs*fixef(spmod)[10])
   lines(ys~xs,lwd=3,col=col_ln)
+  
+  # fixef.low <- as.numeric(modsum$coefs[,1] - 1.96*modsum$coefs[,2])
+  # fixef.high <- as.numeric(modsum$coefs[,1] + 1.96*modsum$coefs[,2])
+  # 
+  # ys <- fixef.low[1] +
+  #   median(spmod$frame$lat)*fixef.low[2] +
+  #   median(spmod$frame$long)*(fixef.low[3] + median(spmod$frame$lat)*fixef.low[8])+
+  #   median(spmod$frame$D)*fixef.low[4] +
+  #   fixef.low[5]*xs +
+  #   median(spmod$frame$hd)*(fixef.low[6] + xs*fixef.low[9]) +
+  #   median(spmod$frame$p.lu)*(fixef.low[7] + xs*fixef.low[10])
+  # lines(ys~xs,lwd=1,lty=2,col=col_ln)
+  # 
+  # ys <- fixef.high[1] +
+  #   median(spmod$frame$lat)*fixef.high[2] +
+  #   median(spmod$frame$long)*(fixef.high[3] + median(spmod$frame$lat)*fixef.high[8])+
+  #   median(spmod$frame$D)*fixef.high[4] +
+  #   fixef.high[5]*xs +
+  #   median(spmod$frame$hd)*(fixef.high[6] + xs*fixef.high[9]) +
+  #   median(spmod$frame$p.lu)*(fixef.high[7] + xs*fixef.high[10])
+  # lines(ys~xs,lwd=1,lty=2,col=col_ln)
+  
   legend('topright', bty='n', legend = paste0('[',low,',',high,']'))
 }
 
