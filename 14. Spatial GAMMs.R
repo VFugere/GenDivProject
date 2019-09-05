f0a7be0d9c2f263d822d36d7c4bba2a07c988fd3
V_Fugere <vincent.fugere@mail.mcgill.ca>
2019-09-05 13:18:04 +0100

    got rid of map model

--- 14. Spatial GAMMs.R
+++ 14. Spatial GAMMs.R
@@ -43,9 +43,15 @@ for(tax in taxa){
       droplevels %>% mutate('wts' = log(nseqs)/mean(log(nseqs))) %>%
       select(-taxon,-scale,-nseqs, -ncomps,-n.years, -lu.var) %>% as.data.frame
     
-    fullmod <- bam(div ~ s(lat,long, bs='gp', k = 50) + s(D, k = 8, bs = 'tp') + s(year,bs = 're', k = 5, m=1) +
-                   s(lat.abs, k = 8, bs = 'tp') + s(hd, k=8, bs ='tp') +
-                   s(p.lu, k=8, bs='tp') + s(order, bs='re',k = 5, m=1) + s(family, bs='re',k = 5, m=1),
+    fullmod <- bam(div ~ 
+                     s(lat,long, bs='gp', k = 50) + 
+                     s(D, k = 8, bs = 'tp') + 
+                     s(year,bs = 're', k = 5, m=1) +
+                     s(lat.abs, k = 8, bs = 'tp') +
+                     s(hd, k=8, bs ='tp') +
+                     s(p.lu, k=8, bs='tp') + 
+                     s(order, bs='re',k = 5, m=1) +
+                     s(family, bs='re',k = 5, m=1),
                    data = temp, family = tw, method='fREML', discrete = T, weights = wts, nthreads = 2)
     
     modname <- paste('m1',tax,scl,sep='_')
